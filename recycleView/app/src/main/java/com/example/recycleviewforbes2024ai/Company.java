140dbe70967366671116b622113d5d3161e53912
Vishwatej1828 <vishwatej.iiit@gmail.com>
2025-01-18 18:50:45 +0530

    app logo and name corrections are added

---
+++ recycleView/app/src/main/java/com/example/recycleviewforbes2024ai/Company.java
@@ -0,0 +1,32 @@
+package com.example.recycleviewforbes2024ai;
+
+
+public class Company {
+    private final String name;
+    private final int imageResId;
+    private final int shortDescription;
+    private final int fullDescription;
+
+    public Company(String name, int imageResId, int shortDescription, int fullDescription) {
+        this.name = name;
+        this.imageResId = imageResId;
+        this.shortDescription = shortDescription;
+        this.fullDescription = fullDescription;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public int getShortDescription() {
+        return shortDescription;
+    }
+
+    public int getFullDescription() {
+        return fullDescription;
+    }
+}
